b83909d47291673571b4061f9d9b236fd2b496b2
Claudio <claudio.junior14@hotmail.com>
2019-11-25 17:22:52 -0300

    initial .Net API

---
+++ Models/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace TodoApi.Models {
+    public class TodoItem {
+        public long Id { get; set; }
+        public string name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
